9991001df8cc58ad865e9a505d0298898c530484
arrias24 <andresarriasc@gmail.com>
2024-06-29 22:10:37 -0400

    feat: Agregada la clase nodo.

---
+++ src/class/node/node.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "./node.hpp"
+using namespace std;
+
+template <class T>
+Node<T>::Node(T data, Node<T>* next){
+    this->data = data;
+    if(next != NULL){
+        this->next = next;
+    }
+}
+
+template <class T>
+T Node<T>::getData(){
+    return this->data;
+}
+
+template <class T>
+void Node<T>::setData(T data){
+    this->data = data;
+}
+
+template <class T>
+Node<T>* Node<T>::getNextNode(){
+    return this->next;
+}
+
+template <class T>
+void Node<T>::setNextNode(Node<T>* next){
+    if(next!=NULL)
+    {
+        this->next=next;
+    }
+}
+
+template <class T>
+void Node<T>::print(){
+    cout<<this->data<<endl;
+}
---
+++ src/class/node/node.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Node{
+    private:
+        T data;
+        Node<T>* next = NULL;
+    public:
+        Node(T data, Node<T>* next);    
+        T getData();
+        void setData(T data);
+        Node<T>* getNextNode();
+        void setNextNode(Node<T>* next);
+        void print();
+};
