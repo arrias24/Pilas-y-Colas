99f35e2f97289d91cdbd302700c72c5aa20e427a
arrias24 <andresarriasc@gmail.com>
2024-07-08 15:41:31 -0400

    fix: Corregido error en generado 'n' cantidad de pilas y colas.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include "../src/class/stack/stack.cpp"
 #include "../src/class/queue/queue.cpp"
 #include "../src/class/list/list.cpp"
@@ -7,107 +8,91 @@ using namespace std;
 
 int main()
 {
+
     int num; // variable contadora
-
-    //COLAS
-
-    cout<<"Cola - Orden Ascendente"<<endl;
-    cout<<"Cuantas colas va a ingresar: ";cin>>num;
-
-    Queue <int> queue_ascending[num]; //creamos las colas
-
-    List  <Queue<int>> list_queue_ascending;  //creamos una lista que va a contener colas que mostraran de forma ascendente.
-
 
     //COLA - ORDEN ASCENDENTE
 
-    for(int i = 0; i < num; i++) //insertamos valores aleatorios a la cola
+    cout<<"Colas - Orden Ascendente"<<endl;
+    cout<<"Cuantas colas quiere ingresar: ";cin>>num;
+
+    
+    List<Queue<int>> list_queue_ascending; //creamos lista que contendra las colas de manera ascendente
+
+    for(int i = 0; i < num; i++) //insertamos 
     {
-        for(int j = 0; j < 5; j++)
-        {
-            queue_ascending[i].push(rand()%100);
-        }
+        Queue<int> queue; // creamos cola
+        queue.push(rand()%100);
+        queue.push(rand()%100);
+        queue.push(rand()%100);
+        queue.push(rand()%100);
+        queue.push(rand()%100);
+        queue.push(rand()%100);
+        list_queue_ascending.insert(queue); //insertamos en la lista
     }
 
-    for (int i = 0; i < num; i++) //insertamos las colas en la lista
-    {
-        list_queue_ascending.insert(queue_ascending[i]);
-    }
     list_queue_ascending.printAscending(); // imprimimos
 
+    //COLA - ORDEN DESCENDENTE
 
-    //COLA - ORDEN DESCENDIENTE
+    cout<<"Colas - Orden Descendente"<<endl;
+    cout<<"Cuantas colas quiere ingresar: ";cin>>num;
 
-    cout<<"Cola - Orden Descendiente"<<endl;
-    cout<<"Cuantas colas va a ingresar: ";cin>>num;
+    List<Queue<int>> list_queue_descending; //creamos lista que contendra colas de manera descendentes
 
-    Queue <int> queue_descending[num]; //creamos las colas
-
-    List  <Queue<int>> list_queue_descending;  //creamos una lista que va a contener colas que se ordenaran de forma descendente.
-
-
-    for(int i = 0; i < num; i++) //insertamos valroes aleatorios
+    for(int i = 0; i < num; i++) //insertamos 
     {
-        for(int j = 0; j < 5; j++)
-        {
-            queue_descending[i].push(rand()%100);
-        }
+        Queue<int> queue; // creamos cola
+        queue.push(rand()%100);
+        queue.push(rand()%100);
+        queue.push(rand()%100);
+        queue.push(rand()%100);
+        queue.push(rand()%100);
+        queue.push(rand()%100);
+        list_queue_descending.insert(queue); //insertamos en la lista
     }
 
-    for (int i = 0; i < num; i++) // insertamos las colas en la lista.
-    {
-        list_queue_descending.insert(queue_descending[i]);
-    }
-    list_queue_descending.printDescending(); // imprimims
-
-    //PILAS
+    list_queue_descending.printDescending(); // imprimimos
 
     //PILA - ORDEN ASCENDENTE
 
-    cout<<"Pila - Orden Ascendente."<<endl;
-    cout<<"Cuantas pilas va a crear: ";cin>>num;
+    cout<<"Pilas - Orden Ascendente"<<endl;
+    cout<<"Cuantas pilas quiere ingresar: ";cin>>num;
 
-    Stack<int> stack_ascending[num]; // creamos pilas
+    List<Stack<int>> list_stack_ascending; // creamos lista que contendra pilas de forma ascendente
 
-    List<Stack<int>> list_stack_ascending; //creamos listas que contendra pilas ascendentes
-
-    for(int i = 0; i < num; i++) // insertamos valores aleatorios a la pila
+    for (int i = 0; i < num; i++)
     {
-        for(int j = 0; j < 5; j++)
-        {
-            stack_ascending[i].push(rand()%100);
-        }
+        Stack<int> stack; // creamos pila
+        stack.push(rand()%100);
+        stack.push(rand()%100);
+        stack.push(rand()%100);
+        stack.push(rand()%100);
+        stack.push(rand()%100);
+        list_stack_ascending.insert(stack); //insertamos en la lista
     }
 
-    for (int i = 0; i < num; i++) // insertamos pilas a la lista
-    {
-        list_stack_ascending.insert(stack_ascending[i]);
-    }
     list_stack_ascending.printAscending(); //imprimimos
 
     //PILA - ORDEN DESCENDENTE
 
+    cout<<"Pilas - Orden Descendente"<<endl;
+    cout<<"Cuantas pilas quiere ingresar: ";cin>>num;
 
-    cout<<"Pila - Orden Descendente."<<endl;
-    cout<<"Cuantas pilas va a crear: ";cin>>num;
+    List<Stack<int>> list_stack_descending; // creamos lista que contendra pilas de forma descendente
 
-    Stack<int> stack_descending[num]; // creamos pilas
-
-    List<Stack<int>> list_stack_descending; //creamos listas que contendra pilas descendentes
-
-    for(int i = 0; i < num; i++) // insertamos valores aleatorios a la pila
+    for (int i = 0; i < num; i++)
     {
-        for(int j = 0; j < 5; j++)
-        {
-            stack_descending[i].push(rand()%100);
-        }
+        Stack<int> stack; // creamos pila
+        stack.push(rand()%100);
+        stack.push(rand()%100);
+        stack.push(rand()%100);
+        stack.push(rand()%100);
+        stack.push(rand()%100);
+        list_stack_descending.insert(stack); //insertamos en la lista
     }
 
-    for (int i = 0; i < num; i++) // insertamos pilas a la lista
-    {
-        list_stack_descending.insert(stack_descending[i]);
-    }
-    list_stack_descending.printDescending(); //imprimimos    
+    list_stack_descending.printDescending(); //imprimimos
 
     return 0;
 }
